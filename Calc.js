f3b41ff26839d49228536a46b3f8b88855f1e7fb
huroy5518 <huroy5518@gmail.com>
2025-10-22 17:10:11 +0800

    Modify Calc to pass test Add

--- Calc.js
+++ Calc.js
@@ -1,5 +1,10 @@
+const { type } = require("os");
+
 class Calc {
     static add(a, b) {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            throw new Error('unsupported operand type');
+        }
         return a + b;
     }
 }
